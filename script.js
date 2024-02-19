69c2be8f85aa6450fb0c8ab5904667cf4d76ec74
Yodgorbekova <marjonayodgorbekova505@gmail.com>
2024-02-19 14:00:05 +0500

    fixed

---
+++ script.js
@@ -0,0 +1,13 @@
+var a = prompt("1-sonni kiriting")
+
+var b = prompt("2-sonni kiriting")
+
+console.log(a,"+",b,"=",+a + + b);
+
+console.log(a,"-",b,"=",a - b);
+
+console.log(a,"*",b,"=",a * b);
+
+console.log(a,"/",b,"=",a / b);
+
+
